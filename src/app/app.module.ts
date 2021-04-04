b90342d85564dbc9fbef1b2080792efd81f9cf7a
alvynah <alvynahw@gmail.com>
2021-04-04 12:40:08 +0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { UserComponent } from './user/user.component';
+import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    UserComponent,
+    RepositoryComponent
   ],
   imports: [
     BrowserModule,
