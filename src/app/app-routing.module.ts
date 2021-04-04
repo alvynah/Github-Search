f4be89200430d09efcf450f24c6784b914545e17
alvynah <alvynahw@gmail.com>
2021-04-04 13:22:51 +0300

    Routing for user and repository menu items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent} from './user/user.component';
+import {RepositoryComponent} from './repository/repository.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'user', component: UserComponent},
+  {path: 'repo', component: RepositoryComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
