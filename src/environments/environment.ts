db668fad669197dbc68c6bdd16a65915061a8060
alvynah <alvynahw@gmail.com>
2021-04-05 10:27:59 +0300

    Add service for search and hiding the access token in environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  accessKey: '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'
 };
 
 /*
