616a1e5cc38093ba84132c29c801a0259787052c
alvynah <alvynahw@gmail.com>
2021-04-06 04:42:39 +0300

    Add pipe to return the number of days ago that a repository was created

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,22 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): unknown {
+    const today: Date = new Date();
+    const todayDateOnly: any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    const dateDifference = Math.abs(todayDateOnly - value);
+    const secondsInDay = 86400;
+    const dateDifferenceSeconds = dateDifference * 0.001;
+    const dayCounter = dateDifferenceSeconds / secondsInDay;
+
+    if (dayCounter <= 0) {
+      return 'today';
+    }
+    if (dayCounter >= 1 && todayDateOnly > value) {
+      return dayCounter;
+    } else {
+      return 0;
+    }
   }
 
 }
